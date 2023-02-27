731a21f58855807e5f7fef0886db0a818baf6300
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-26 19:25:04 -0800

    Add created_by tags to all resources

--- modules/state-bucket/variables.tf
+++ modules/state-bucket/variables.tf
@@ -2,3 +2,9 @@ variable "bucket" {
   description = "Bucket name for a Terraform state"
   type        = string
 }
+
+variable "tags" {
+  description = "Tags to apply on S3 bucket"
+  type = map(string)
+  default = {}
+}
