ae29240aa221ed67fd14bb28cfea321225ce9ee6
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-02-26 19:57:50 -0800

    Add pre-commit hook

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ locals {
   state_buckets = {
     "infrahouse-aws-s3-control" : {}
     "infrahouse-github-state" : {}
-    "infrahouse-website-infra": {}
+    "infrahouse-website-infra" : {}
   }
 }
 
